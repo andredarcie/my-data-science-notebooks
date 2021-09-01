c54debb2a0448c16810de2305e0114892e9b68e2
andrendarcie <andrendarcie@gmail.com>
2021-08-31 21:35:09 -0300

    Create dlib_svm_train.py

---
+++ analise-e-classificacao-de-imagens/tarefa_5/dlib_svm_train.py
@@ -0,0 +1,10 @@
+import dlib
+
+# Gerar os arquivos XML
+# https://imglab.in/
+
+opcoes = dlib.simple_object_detector_training_options()
+opcoes.add_left_right_image_flips = True
+opcoes.C = 5
+
+dlib.train_simple_object_detector("res/treinamentoimagens.xml", "res/detectorimagens.svm", opcoes)
