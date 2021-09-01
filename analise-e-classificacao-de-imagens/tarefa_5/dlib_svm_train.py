4179e05158640e4a97e3ede334673c7cfbd16489
andrendarcie <andrendarcie@gmail.com>
2021-08-31 21:56:39 -0300

    Update dlib_svm_train.py

--- analise-e-classificacao-de-imagens/tarefa_5/dlib_svm_train.py
+++ analise-e-classificacao-de-imagens/tarefa_5/dlib_svm_train.py
@@ -7,4 +7,5 @@ opcoes = dlib.simple_object_detector_training_options()
 opcoes.add_left_right_image_flips = True
 opcoes.C = 5
 
+# Treinamento
 dlib.train_simple_object_detector("res/treinamentoimagens.xml", "res/detectorimagens.svm", opcoes)
